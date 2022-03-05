ec274a2efa07883921f70ba4aae91559a4027cb7
C M Pandey <cm@cmcodes.in>
2022-03-05 21:44:01 +0530

    🎨 folder structure created

---
+++ src/screens/Item.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Item() {
+    return (
+        <View>
+            <Text>Item</Text>
+        </View>
+    )
+}
